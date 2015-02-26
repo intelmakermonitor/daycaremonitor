79eedf693122e10b94ddc11bb02fda5c7878a2f2
Kevin Zhang <kzhang01@gmail.com>
2015-02-26 05:14:08 +0000

    add calculateDistance() to get the distance reading based on rssi
    This is based on the following links:
    http://blogs.truthlabs.com/2014/12/16/proximity-tracking-with-node-js-and-raspberry-pi/
    and:
    https://gist.github.com/eklimcz/446b56c0cb9cfe61d575

--- tools/noble-ble/scanner_client.js
+++ tools/noble-ble/scanner_client.js
@@ -1,3 +1,20 @@
+function calculateDistance(rssi) {
+  var txPower = -59 //hard coded power value. Usually ranges between -59 to -65
+  
+  if (rssi == 0) {
+    return -1.0; 
+  }
+ 
+  var ratio = rssi*1.0/txPower;
+  if (ratio < 1.0) {
+    return Math.pow(ratio,10);
+  }
+  else {
+    var distance =  (0.89976)*Math.pow(ratio,7.7095) + 0.111;    
+    return distance;
+  }
+}
+
 var BTDevices = ["a4d856039ebf", "ee443390fa9d", "fb738cf43b8a"];
 
 var noble = require('noble');
@@ -20,6 +37,7 @@ noble.on('discover', function(peripheral){
 //      socket.emit('deviceData', {mac: peripheral.uuid, rssi:peripheral.rssi});
       socket.emit('uuid', peripheral.uuid);
       socket.emit('rssi', peripheral.rssi);
+      console.log('distance is: ', calculateDistance(peripheral.rssi));
     }
   }
 });
